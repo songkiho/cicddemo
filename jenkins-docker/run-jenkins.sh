e5e9c41cdc3c80450f490a4a81bae55938c4d9af
uvelyster <48861250+uvelyster@users.noreply.github.com>
2021-10-01 11:51:17 +0900

    Update run-jenkins.sh

--- jenkins-docker/run-jenkins.sh
+++ jenkins-docker/run-jenkins.sh
@@ -6,6 +6,6 @@ docker run -d --name jenkins \
 --dns 172.16.0.200 \
 -p 8080:8080 \
 -p 50000:50000 \
--v /root/jenkins:/var/jenkins_home \
+-v /root/jenkins_home:/var/jenkins_home \
 -v /var/run/docker.sock:/var/run/docker.sock \
 jenkins-docker
