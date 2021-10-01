8d3df631a4a6dbd552b3d174df2d3d1d79c4d50b
uvelyster <48861250+uvelyster@users.noreply.github.com>
2021-10-01 11:46:44 +0900

    Create run-jenkins.sh

---
+++ jenkins-docker/run-jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run -d --name jenkins \
+--dns 172.16.0.200 \
+-p 8080:8080 \
+-p 50000:50000 \
+-v /root/jenkins:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+jenkins-docker
